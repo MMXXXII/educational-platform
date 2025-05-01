aca695e503635533414909b23cf2d96c5cfa65e2
DreamCutter28 <martirosovsergey777@gmail.com>
2025-05-01 20:26:09 +0800

    .env vite fix

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,5 @@
 const config = {
-    //В vite env константы начинаются с VITE а не с REACT. Возможно стоит заменить
-    apiUrl: process.env.REACT_APP_API_URL,
+    apiUrl: import.meta.env.VITE_API_URL,
 };
 
 export default config;
